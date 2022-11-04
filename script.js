e10aaf3a311ded0949644eaf229228a5fc7e837b
Cherry Ella <111384784+CherryElla@users.noreply.github.com>
2022-11-03 22:21:34 -0400

    created an array for entries objects, created rows dynamically, created some local sotrage functions

--- script.js
+++ script.js
@@ -1,21 +1,84 @@
 // Current time and day
-setInterval(function () {    
-    let today = moment().format("MMMM Do YYYY, h:mm:ss a")   
-    $("#date").text(today) ;
-}, 1000)
+setInterval(function () {
+    let today = moment().format("MMMM Do YYYY, h:mm:ss a");
+    $("#date").text(today);
+}, 1000);
 
-
+// Constants for HTML elements
 const plannerForm = document.getElementById("planner-form");
 const plannerInput = document.getElementsByClassName("planner-input");
-const plannerSubmit = document.getElementById("submit-button");
-const plannerSavedNote = document.getElementById("notes");
+const submitButton = document.getElementById("submit-button");
+const savedNote = document.getElementById("notes");
+
+// Entries array of objects with time and description keys
+let entries = [
+    { time: "9:00am", description: "" },
+    { time: "10:00am", description: "Here is some stuff" },
+    { time: "11:00am", description: "" },
+    { time: "12:00pm", description: "" },
+    { time: "1:00pm", description: "" },
+    { time: "2:00pm", description: "" },
+];
 
 
 
-// Function for the lock button
-function saveEventEntered (event) {
 
+
+
+
+function readLocalStorage(key) {
+    return JSON.parse(localStorage.getItem(key));
 }
 
-// Event listeners for the lock button press
-let allLockButtons = document.querySelector("#padlock").addEventListener("click", saveEventEntered)
+function writeLocalStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+function submitClicked(event) {
+    console.log("clicked");
+    console.log(event.target);
+}
+
+ // Inserting rows
+function addRow(idx, entryObject) {
+    let row = `
+    <div class="col-xs-2 col-2">${entryObject.time}</div>
+    <div id="eventText" class="col-xs-8 col-md-8">
+        <form id="planner-form">
+            <input 
+                name="new-event"
+                type="text" 
+                class="planner-input w-100" 
+                placeholder="Add note here" 
+                value="${entryObject.description}">
+        </form>
+        <ul id="notes"></ul>
+    </div>
+    <div class="col-xs-2 col-2">
+        <button id="btn-${idx}" 
+            entryIndex="${idx}"
+            type="button"
+            class="btn btn-primary"
+        >
+            <img id="img-${idx}" 
+            entryIndex="${idx}"
+            src="submitImg.png"/>
+        </button>
+`;
+    let mainContainer = document.querySelector("#main-container");
+    let rowEl = document.createElement("div");
+    rowEl.id = `entry-${idx}`;
+    rowEl.classList = ["row"];
+    rowEl.innerHTML = row;
+    mainContainer.append(rowEl);
+
+    let btnId = `#btn-${idx}`;
+    let btn = document.querySelector(btnId);
+    btn.addEventListener("click", submitClicked);
+}
+
+
+
+for (let i = 0; i < entries.length; i++) {
+    addRow(i, entries[i]);
+}
